df74195ab1e4cad523b8a7b98a7c4092f9709ce1
Stephen Ramsay <rabrandom@gmail.com>
2020-03-09 16:44:17 +0000

    table is finished and working and clickable

--- Components/BookingsListItem.js
+++ Components/BookingsListItem.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import BookingDetails from './BookingDetails';
 import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 
 const BookingsListItem = ({ bookings }) => {
@@ -9,19 +8,20 @@ const BookingsListItem = ({ bookings }) => {
         console.log(pressedItem)
     }
 
-    const [tableHead, setTableHead] = useState(['time', 'name', 'table'])
+    const [tableHead, setTableHead] = useState(['time', 'name', 'table', 'arrived?', 'left?'])
     const [tableData, setTableData] = useState([])
 
     const tableDataNodes = bookings.map((booking) => {
-        return (<Row onPress={() => handlePress({ booking })} data={[booking.startTime, booking.customer.name, booking.eatingPlatformId]} />)
+        return (<TouchableOpacity onPress={() => handlePress(booking)} >
+            <Row data={[booking.startTime, booking.customer.name, booking.eatingPlatformId, 'button', 'button']} />
+        </TouchableOpacity>
+        )
     })
 
     return (
         <View>
             <Table borderStyle={{ borderWidth: 2, borderColor: '#c8e1ff' }}>
-            <TouchableOpacity>
                 <Row data={tableHead} />
-                </TouchableOpacity>
                 {tableDataNodes}
             </Table>
 
